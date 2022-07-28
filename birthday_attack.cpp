47bd7ce86e26e654db6b83c9990499169fd4ee0d
bierg <72506437+jlx77777@users.noreply.github.com>
2022-07-28 14:43:11 +0800

    Update birthday_attack.cpp

--- birthday_attack.cpp
+++ birthday_attack.cpp
@@ -1,8 +1,9 @@
-﻿#include <openssl/evp.h>
+#include<openssl/evp.h>
 #include<iostream>
 #include<map>
+#include<ctime>
 using namespace std;
-bool openssl_sm3_hash(const unsigned char *input,unsigned char* buffer,unsigned int* buf_len)
+bool openssl_sm3_hash(const unsigned char* input, unsigned char* buffer, unsigned int* buf_len)
 {
     memset(buffer, 0, *buf_len);
     EVP_MD_CTX* ctx = EVP_MD_CTX_new();
@@ -10,7 +11,7 @@ bool openssl_sm3_hash(const unsigned char *input,unsigned char* buffer,unsigned 
         cout << "Failed to init" << endl;
         return false;
     }
-    if (!EVP_DigestUpdate(ctx, input,64)) {
+    if (!EVP_DigestUpdate(ctx, input, 64)) {
         cout << "Failed to update" << endl;
         return false;
     }
@@ -23,11 +24,10 @@ bool openssl_sm3_hash(const unsigned char *input,unsigned char* buffer,unsigned 
     EVP_MD_CTX_free(ctx);
     return true;
 }
-//map<unsigned int, map<unsigned int, map<unsigned int, map<unsigned int,bool>>>> f;//128bit
 map<unsigned int, map<unsigned int, int>> f;//64bit
 //map<unsigned int, int> f;//32bit
 
-const bool CHECK=0;
+const bool CHECK = 0;
 
 bool check_32bit(unsigned int, unsigned int);
 bool check_64bit(unsigned int, unsigned int);
@@ -46,9 +46,11 @@ int main() {
     }
     unsigned char s[64];
     unsigned char buf[100];
-    unsigned int len=64 ;
+    unsigned int len = 64;
     srand(19260817);
-    for (int t = 0; 1;t++) {
+    time_t begin, end;
+    begin = clock();
+    for (int t = 0; 1; t++) {
         if (t % 1000000 == 0) {
             printf("%d\n", t / 1000000);
         }
@@ -58,7 +60,7 @@ int main() {
         openssl_sm3_hash(s, buf, &len);
         //64bit
         unsigned int a = *((int*)buf), b = *((int*)buf + 1);
-        if (f.find(a) != f.end() && f[a].find(b) != f[a].end() ) {
+        if (f.find(a) != f.end() && f[a].find(b) != f[a].end()) {
             printf("found %d,%d\n", f[a][b], t);//0 134217728
             break;
         }
@@ -75,6 +77,8 @@ int main() {
             f[a] = t;
         }*/
     }
+    end = clock();
+    cout << (end - begin) / 1000 << 's' << endl;
     return 0;
 }
 bool check_32bit(unsigned int x, unsigned int y) {
@@ -83,14 +87,14 @@ bool check_32bit(unsigned int x, unsigned int y) {
     unsigned int len = 64;
     srand(19260817);
     if (x > y) swap(x, y);
-    unsigned int a=0, b=0;
+    unsigned int a = 0, b = 0;
     for (int i = 0; i < 100000000; i++) {
         if (i != x && i != y) {
             for (int i = 0; i < 16; i++) {
                 rand();
             }
             continue;
-        }          
+        }
         for (int i = 0; i < 16; i++) {
             *((int*)s + i) = rand();
         }
@@ -129,4 +133,3 @@ bool check_64bit(unsigned int x, unsigned int y) {
     }
     return a == b;
 }
-
